369d8c5df1ecb058da19c6d4f97476b2b384b08d
szczekulskij <szczekulskij@gmail.com>
2023-07-31 01:48:57 +0100

    update css

--- src/components/skillProgressSection/linearProgressBar.js
+++ src/components/skillProgressSection/linearProgressBar.js
@@ -13,6 +13,8 @@ const useStyles = makeStyles({
       "& .MuiLinearProgress-barColorPrimary": {
           backgroundColor: "#b58500",
       },
+      marginBottom: "0px",
+      paddingBottom: "0px"
   },
 });
 
@@ -21,10 +23,10 @@ export default function LinearProgressBar(props) {
   const classes = useStyles();
   return (
     <Container>
-    <Typography variant="body2" color="white">{props.name}</Typography>
-    <Box sx={{ display: 'flex', alignItems: 'center' }}>
-      <Box sx={{ width: '88%', mr: 1 }} className={classes.root}>
-        <LinearProgress variant="determinate" value={props.progress} sx={{height: 13}}/>
+    <Typography variant="overline" color="white" >{props.name}</Typography>
+    <Box sx={{ display: 'flex', alignItems: 'center', paddingBottom:"6px", paddingTop:"0px", marginTop:"0px"}}>
+      <Box sx={{ width: '88%', mr: 1 }}>
+        <LinearProgress variant="determinate" value={props.progress} sx={{height: 7}}/>
       </Box>
       <Box sx={{ minWidth: 3}}>
         <Typography variant="body2" color="white">{`${Math.round(props.progress,)}%`}</Typography>

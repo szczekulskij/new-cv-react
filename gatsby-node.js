97eb6965381ebdb679bc593071e315cd212a2a0a
szczekulskij <szczekulskij@gmail.com>
2024-07-07 19:30:54 -0700

    add a 1st draft of a cycleGAN blog post

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,4 @@
-const path = require('path');
+  const path = require('path');
 const { createFilePath } = require('gatsby-source-filesystem');
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
